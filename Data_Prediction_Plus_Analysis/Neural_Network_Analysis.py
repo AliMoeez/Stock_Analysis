4c013afe741a88f4cb5e3eed7d0618def47ff0ca
Moeez <oct30moeez@gmail.com>
2023-10-12 12:35:27 -0400

    Worked On Neural Netowrk Analysis For Dashboard

--- Data_Prediction_Plus_Analysis/Neural_Network_Analysis.py
+++ Data_Prediction_Plus_Analysis/Neural_Network_Analysis.py
@@ -1,4 +1,9 @@
 import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import *
+from tensorflow.keras.losses import MeanSquaredError
+from tensorflow.keras.metrics import RootMeanSquaredError
+from tensorflow.keras.optimizers import Adam
 
 import pandas as pd
 import numpy as np
@@ -15,8 +20,34 @@ class NeuralNetowrkAnalysis:
         self.df_scotia_div=pd.read_csv(r"Data\Dividends\BNS.TO (1).csv",engine="pyarrow") ; self.df_cibc_div=pd.read_csv(r"Data\Dividends\CIBC.TO (1).csv",engine="pyarrow") 
         self.df_naboc_div=pd.read_csv(r"Data\Dividends\NABOC.TO (1).csv",engine="pyarrow") ; self.df_td_div=pd.read_csv(r"Data\Dividends\TD.TO (1).csv",engine="pyarrow")
 
+    def add_timeline(self):
+        
+        self.x=0
+        self.time_list=[]
+        for i in range(len(self.df_bmo["Date"])):
+            self.x+=1
+            self.time_list.append(self.x)
+        
+        self.df_bmo["Time"]=self.time_list
 
+    def model(self):
+        self.model=Sequential()
+        self.model.add(InputLayer((5,1)))
+        self.model.add(LSTM(64))
+        self.model.add(Dense(8,"relu"))
+        self.model.add(Dense(1,"linear"))
+
+    def model_compile(self):
+        self.model.compile(loss=MeanSquaredError(),optimizer=Adam(learning_rate=0.0001),metrics=[RootMeanSquaredError()])
+        self.model.fit(self.df_bmo["Time"],self.df_bmo["Open"],epochs=10)
+
+    def model_predict(self):
+        self.model_predict=self.model.predict(self.df_bmo["Open"])
 
 
 
 neuralnetworkanalysis=NeuralNetowrkAnalysis()
+neuralnetworkanalysis.add_timeline()
+neuralnetworkanalysis.model()
+neuralnetworkanalysis.model_compile()
+neuralnetworkanalysis.model_predict()
